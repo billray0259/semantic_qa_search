011bee4049e35d6db71d505978037b241bf66512
Bill Ray <bill.ray0259@gmail.com>
2022-03-02 12:24:09 -0500

    Now in the browser with images of the slides

--- lib/pdf_converter.py
+++ lib/pdf_converter.py
@@ -1,13 +1,14 @@
-# Copied from
-# https://stackoverflow.com/questions/28246161/pdfminer-export-pages-as-list-of-strings
-
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from io import StringIO
 
+import fitz
+import os
+
 def pdf_to_txt_list(file_path):
+    # https://stackoverflow.com/questions/28246161/pdfminer-export-pages-as-list-of-strings
     # Returns a list of strings, one for each page in the PDF file.
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
@@ -39,3 +40,25 @@ def pdf_to_txt_list(file_path):
             pages_text.append(page_text)
         
     return pages_text
+
+
+def pdf_to_img_list(file_name, pages, target_directory):
+    if not os.path.exists(target_directory):
+        os.makedirs(target_directory)
+    # https://stackoverflow.com/questions/46184239/extract-a-page-from-a-pdf-as-a-jpeg
+    saved_files = []
+    doc = fitz.open(file_name)
+    name = os.path.splitext(os.path.basename(file_name))[0]
+    for i in pages:
+        try:
+            page = doc.load_page(i-1)
+        except ValueError:
+            print(f"Page {i} does not exist in {file_name}")
+        zoom = 2
+        mat = fitz.Matrix(zoom, zoom)
+        pix = page.get_pixmap(matrix=mat)
+        save_name = f"{name}_{i}.jpg"
+        pix.save(os.path.join(target_directory, save_name))
+        saved_files.append(save_name)
+        
+    return saved_files
